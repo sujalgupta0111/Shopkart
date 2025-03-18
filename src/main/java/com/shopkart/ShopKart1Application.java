0694d9ff7d1f4efe6f91c644bb3fbddb594fec52
Sujal Gupta <sujalguptapro0111@gmail.com>
2025-03-18 13:19:13 +0530

    Update ShopKart1Application.java

--- src/main/java/com/shopkart/ShopKart1Application.java
+++ src/main/java/com/shopkart/ShopKart1Application.java
@@ -1,1 +1,14 @@
 
+package com.shopkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopKart1Application.class, args);
+	}
+
+}
